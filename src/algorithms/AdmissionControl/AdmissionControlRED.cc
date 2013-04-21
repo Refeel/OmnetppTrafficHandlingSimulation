164d85b9e44b485a0b1dc5974fd00b221486cdce
piecset <maciej.mastalerz@gmail.com>
2013-04-21 22:47:04 +0200

    RED ac

---
+++ src/algorithms/AdmissionControl/AdmissionControlRED.cc
@@ -0,0 +1,57 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "AdmissionControlRED.h"
+
+namespace omnetpptraffichandlingsimulation {
+
+void AdmissionControlRED::handleMessage(cMessage *msg) {
+
+    if(msg==this->msgServiced)
+    {
+        if(packetQueue.isEmpty())
+        {
+            isMsgServiced = false;
+        }
+        else
+        {
+
+            simtime_t serviceTime = serviceMsg(check_and_cast<SimplePacket *>(packetQueue.front()));
+            packetQueue.pop();
+            isMsgServiced = true;
+            scheduleAt(simTime() + serviceTime, msgServiced);
+        }
+    }
+    else if(!isMsgServiced) {
+        simtime_t serviceTime = serviceMsg(check_and_cast<SimplePacket *> (msg));
+        isMsgServiced = true;
+        scheduleAt(simTime() + serviceTime, msgServiced);
+    }
+    else {
+            int queueLength = packetQueue.length();
+            double dropProbability = ((double)intuniform(0, 100)) / 100;
+
+            if(dropProbability <= ((double)queueLength)/maxPacketsInQueue) {
+                delete(msg);
+                ev << "RED: Delete Message" << endl;
+            }
+            else{
+                packetQueue.insert(check_and_cast<SimplePacket *> (msg));
+            }
+    }
+
+}
+
+} /* namespace omnetpptraffichandlingsimulation */
